775e218a21fa686ca4db33c009b3f613a9263342
Louis Li <louisrli@gmail.com>
2020-05-02 07:36:32 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// Q1: fetche the data from bootcamp.json
+
+
+// Q2: write a function that return a new array with the students first names with "-" before each one of them and display it in the div "studentsFirstName" using map
+
+
+
+
+// Q3: write a function that returns an array with students names that starts with the letter 'm' using filter display it in the div "studentsNamesStartWthM"
+
+
+
+
+// Q4: write a function that returns a number of students students that their first name start with the letter 'e' using reduce and display it in div "NumberOfStudentsNamesStartsWithE"
+
+
+
+
+
+// Q5: write a function that returns the index of the first student name that starts with the letter 'h' and display it in the div "IndexOfFirstStudentNameStartsWithH"
+
+
+
+
+
+// Q6: write a function that adds the instructers array to the beginning of the students array and returns a new array called everybody, console log the new array, use a spread operator to do it
+
+
+
+
+// Q7: write a function to update the key: "title" to the value "Re:Coded Istanbul Bootcamp" in the object in bootcamp.json file
+
+
+
+
+
+// Q8: write a function to add the key: "program manager" and the value "Jaime Mikush" to the object in bootcamp.json file
+
+
+
+// good luck 😈
